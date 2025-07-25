9d48276e90351d263d1d5d2627bec6cf37274727
fatimamoha187 <fmoha187@gmail.com>
2025-07-25 20:58:39 +0300

    added intro files

---
+++ main.js
@@ -0,0 +1,23 @@
+
+   var swiper = new Swiper(".mySwiper", {
+    loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+    
+
+document.getElementById('categoryLink').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent default anchor click behavior
+  const categories = document.getElementById('categories');
+  categories.style.display = categories.style.display === 'none' || categories.style.display === '' ? 'block' : 'none';
+});
