8c64a0b6b9037acd56b650e59fb679c9cc3688fb
fatimamoha187 <fmoha187@gmail.com>
2025-08-15 17:23:05 +0300

    added men,perfumes,shoeshomedecor

---
+++ perfumes/includes/sidebar.php
@@ -0,0 +1,147 @@
+<!-- Sidebar Filters -->
+<aside class="sidebar">
+    <div class="sidebar-header">
+        <h3>Refine By</h3>
+        <span class="style-count">4146 Styles</span>
+    </div>
+          <div class="side">
+     <div class="filter-section">
+         <!-- Gender Filter -->
+         <div class="filter-group">
+             <div class="filter-header">
+                 <h4>Gender</h4>
+             </div>
+             <div class="filter-options">
+                 <label class="filter-option">
+                     <input type="checkbox" name="gender[]" value="women">
+                     <span class="checkmark"></span>
+                     Women
+                 </label>
+                 <label class="filter-option">
+                     <input type="checkbox" name="gender[]" value="men">
+                     <span class="checkmark"></span>
+                     Men
+                 </label>
+                 
+             </div>
+         </div>
+     </div>
+
+     <div class="filter-section">
+         <!-- Size Filter -->
+        <div class="filter-group">
+            <div class="filter-header">
+                <h4>Size</h4>
+            </div>
+            <div class="filter-options">
+                <div class="size-grid">
+                    <label class="filter-option">
+                        <input type="checkbox" name="size[]" value="100ml">
+                        <span class="checkmark"></span>
+                        100ml
+                    </label>
+                    <label class="filter-option">
+                        <input type="checkbox" name="size[]" value="50ml">
+                        <span class="checkmark"></span>
+                        50ml
+                    </label>
+                    <label class="filter-option">
+                        <input type="checkbox" name="size[]" value="30ml">
+                        <span class="checkmark"></span>
+                        30ml
+                    </label>
+                    
+                </div>
+            </div>
+        </div>
+
+        <!-- Category Filter -->
+    <div class="filter-section">
+        <div class="filter-group">
+            <div class="filter-header">
+                <h4>Brands</h4>
+            </div>
+            <div class="filter-options">
+                <label class="filter-option">
+                    <input type="checkbox" name="category[]" value="chanel">
+                    <span class="checkmark"></span>
+                    Chanel
+                </label>
+                <label class="filter-option">
+                    <input type="checkbox" name="category[]" value="dior">
+                    <span class="checkmark"></span>
+                    Dior
+                </label>
+                <label class="filter-option">
+                    <input type="checkbox" name="category[]" value="lattafa">
+                    <span class="checkmark"></span>
+                    Lattafa
+                </label>
+                <label class="filter-option">
+                    <input type="checkbox" name="category[]" value="swiss-arabian">
+                    <span class="checkmark"></span>
+                    Swiss Arabian
+                </label>
+                <label class="filter-option">
+                    <input type="checkbox" name="category[]" value="valentino">
+                    <span class="checkmark"></span>
+                    Valentino
+                </label>
+                <label class="filter-option">
+                    <input type="checkbox" name="category[]" value="gucci">
+                    <span class="checkmark"></span>
+                    Gucci
+                </label>
+                <label class="filter-option">
+                    <input type="checkbox" name="category[]" value="other">
+                    <span class="checkmark"></span>
+                    Other
+                </label>
+            </div>
+        </div>
+    </div>
+
+        
+    </div>
+
+    <!-- Price Filter -->
+    <div class="filter-section">
+        <div class="filter-group">
+            <div class="filter-header">
+                <h4>Price</h4>
+            </div>
+            <div class="filter-options">
+                <label class="filter-option">
+                    <input type="checkbox" name="price[]" value="90-110">
+                    <span class="checkmark"></span>
+                    $90 - $110
+                </label>
+                <label class="filter-option">
+                    <input type="checkbox" name="price[]" value="110-150">
+                    <span class="checkmark"></span>
+                    $110 - $150
+                </label>
+                <label class="filter-option">
+                    <input type="checkbox" name="price[]" value="150-200">
+                    <span class="checkmark"></span>
+                    $150 - $200
+                </label>
+                <label class="filter-option">
+                    <input type="checkbox" name="price[]" value="200-250">
+                    <span class="checkmark"></span>
+                    $200 - $250
+                </label>
+                <label class="filter-option">
+                    <input type="checkbox" name="price[]" value="250+">
+                    <span class="checkmark"></span>
+                    $250+
+                </label>
+            </div>
+        </div>
+    </div>
+
+    
+
+    
+    </div>
+</aside> 
